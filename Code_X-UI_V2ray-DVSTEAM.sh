763c8011a8b76fd7d6b7de2f4245a6f85dc18949
dvsteam <91702958+dvsteam@users.noreply.github.com>
2021-10-20 01:36:42 +0700

    Update Code_X-UI_V2ray-DVSTEAM.sh

--- Code_X-UI_V2ray-DVSTEAM.sh
+++ Code_X-UI_V2ray-DVSTEAM.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+red='\033[0;33m'
+green='\033[0;35m'
+yellow='\033[0;34m'
+plain='\033[0m'
+
 echo " https://vpn.dvsteam.tk "
 apt-get update -y
 bash <(curl -Ls https://raw.githubusercontent.com/dvsteam/DVS/main/X-UI-DVS-TEAM.sh)
@@ -9,10 +14,10 @@ ufw allow 443
 echo " Mở Port 80 "
 ufw allow 80
 ufw allow 80/tcp
-echo " DVS TEAM Cài Đặt  X-UI Thành Công "  
-echo " Truy Cập Vào Trình duyệt Web Nhập (Địa Chỉ IPv4)của VPS hoặc (Tên Miền) Thêm Đuôi (:54321) "
-echo " Tài Khoảng(Use)=> admin "
-echo " Mật Khẩu(pass)=> admin "
-echo " Sau Khi Đăng Nhập Vui Lòng Đổi Mật Khẩu(pass) để tránh bị lộ thông tin"
-echo " Tham Gia Nhóm Hỗ Trợ Tại: https://vpn.dvsteam.tk "
-echo " Copyright © DVS TEAM "
+echo "${yellow}DVS TEAM Cài Đặt  X-UI Thành Công "  
+echo "Truy Cập Vào Trình duyệt Web Nhập (Địa Chỉ IPv4)của VPS hoặc (Tên Miền) Thêm Đuôi (:54321) "
+echo "${green}Tài Khoảng(Use)=> admin${plain}"
+echo "${green}Mật Khẩu(pass)=> admin${plain}  "
+echo "${yellow}Sau Khi Đăng Nhập Vui Lòng Đổi (Mật Khẩu) để tránh bị lộ thông tin${plain} "
+echo "Tham Gia Nhóm Hỗ Trợ Tại: ${yellow}https://vpn.dvsteam.tk${plain}"
+echo "Copyright © DVS TEAM "
