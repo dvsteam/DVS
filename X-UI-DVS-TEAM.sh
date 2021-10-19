66dfb02b630ce177eb67ce63a25def70d35d4ea2
dvsteam <91702958+dvsteam@users.noreply.github.com>
2021-10-20 03:06:43 +0700

    Update X-UI-DVS-TEAM.sh

--- X-UI-DVS-TEAM.sh
+++ X-UI-DVS-TEAM.sh
@@ -147,7 +147,7 @@ install_x-ui() {
     echo -e "》x-ui install      ➪ ${xanh}Cài đặt bảng điều khiển x-ui${plain}"
     echo -e "》x-ui uninstall    ➪ ${xanh}Gỡ cài đặt bảng điều khiển x-ui${plain}"
     echo -e "»Truy Cập Vào ${yellow}(https://vpn.dvsteam.tk)${plain} để bít thêm chi tiếc và Gruop. "
-    echo -e "$${yellow}•{xanh}---------------------------------------------${yellow}• "
+    echo -e "$${yellow}•${xanh}---------------------------------------------${yellow}• "
     echo -e "${yellow}© Dương Văn Sỹ - Zalo:0835315551"
     echo -e "${yellow}•${xanh}---------------------------------------------${yellow}• "
 }
