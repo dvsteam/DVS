c59d3c134ebdfd3dbd5925be56683513b3aabd4b
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2021-10-20 06:57:09 +0700

    Update install_X-UI_DVSTEAM.sh

--- install_X-UI_DVSTEAM.sh
+++ install_X-UI_DVSTEAM.sh
@@ -1,6 +1,7 @@
 # DƯƠNG VĂN SỸ - ZALO: 0835315551 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Chú ý：${plain} Tập lệnh này phải được chạy với tư cách người dùng gốc(root)！\n" && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${red}Chú ý：${plain}Tập lệnh này phải được chạy với tư cách người dùng gốc(root)! \n ${xanh}➫Vui Lòng Gõ Lệnh: ${yellow}sudo -i \n ➬Để Kích Hoạt Root, và thử lại lần nữa \n ${xanh}〖DVS TEAM〗『https://vpn.dvsteam.tk』 " && exit 1
+# DVS
 apt-get update -y
 bash <(curl -Ls https://raw.githubusercontent.com/dvsteam/DVS/main/X-UI-DVS-TEAM.sh)
 echo "》『Mở Port: 54321』《 "
