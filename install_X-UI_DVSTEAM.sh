065e93c1c6b11e88d0676a3afa959f71e9c79c4b
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2021-10-20 04:24:01 +0700

    Update install_X-UI_DVSTEAM.sh

--- install_X-UI_DVSTEAM.sh
+++ install_X-UI_DVSTEAM.sh
@@ -2,14 +2,14 @@
 
 apt-get update -y
 bash <(curl -Ls https://raw.githubusercontent.com/dvsteam/DVS/main/X-UI-DVS-TEAM.sh)
-echo "》Mở Port 54321《 "
+echo "》『Mở Port: 54321』《 "
 ufw allow 54321
-echo "》Mở Port 443《 "
+echo "》『Mở Port: 443』《 "
 ufw allow 443
-echo "》Mở Port 80《 "
+echo "》『Mở Port: 80』《 "
 ufw allow 80
 echo "----------------------------------------------"
-echo "✔•DVS TEAM Cài Đặt  X-UI Thành Công•✔ "  
+echo "✔•DVS TEAM Hoàn Tất Cài Đặt X-UI•✔ "  
 echo "---------------------------------------------- "
 echo "1.Truy Cập Vào Trình duyệt (Chrome) hoặc (Cốc Cốc) "
 echo "2.Nhập『Địa Chỉ IPv4』của VPS hoặc『Tên Miền』Thêm Đuôi (:54321) "
