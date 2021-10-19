0e73c250eb7abd981592538331c618fe1a41ca22
dvsteam <91702958+dvsteam@users.noreply.github.com>
2021-10-20 01:27:12 +0700

    Toàn Bộ Lệnh Chạy X-UI

--- Code_X-UI_V2ray-DVSTEAM.sh
+++ Code_X-UI_V2ray-DVSTEAM.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo " https://vpn.dvsteam.tk "
 apt-get update -y
-bash <(curl -Ls https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/dvsteam/DVS/main/X-UI-DVS-TEAM.sh)
 echo " Mở Port 54321 "
 ufw allow 54321
 echo " Mở Port 443 "
@@ -9,7 +9,7 @@ ufw allow 443
 echo " Mở Port 80 "
 ufw allow 80
 ufw allow 80/tcp
-echo " Cài Đặt Thành Công X-UI "  
+echo " DVS TEAM Cài Đặt  X-UI Thành Công "  
 echo " Truy Cập Vào Trình duyệt Web Nhập (Địa Chỉ IPv4)của VPS hoặc (Tên Miền) Thêm Đuôi (:54321) "
 echo " Tài Khoảng(Use)=> admin "
 echo " Mật Khẩu(pass)=> admin "
