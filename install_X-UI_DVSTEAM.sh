b84b626195988c486342ccea4f01167288702507
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2021-10-20 08:28:19 +0700

    Update install_X-UI_DVSTEAM.sh

--- install_X-UI_DVSTEAM.sh
+++ install_X-UI_DVSTEAM.sh
@@ -6,7 +6,7 @@ tim='\033[1;35m'
 plain='\033[0m'
 cur_dir=$(pwd)  
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Chú ý：${plain}Tập lệnh này phải được chạy với tư cách người dùng gốc(root)! \n ${xanh}➫Vui Lòng Gõ Lệnh: ${yellow}sudo -i \n ➬Để Kích Hoạt Root, và thử lại lần nữa \n ${xanh}〖DVS TEAM〗『https://vpn.dvsteam.tk』 " && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${tim}Chú ý：${plain}Tập lệnh này phải được chạy với tư cách người dùng gốc ${tim}(root)! \n ${tim}➫Vui Lòng Gõ Lệnh: ${yellow}sudo -i \n ➬Để Kích Hoạt Root, và thử lại lần nữa \n ${vang}〖DVS TEAM〗『https://vpn.dvsteam.tk』${plain} " && exit 1
 # Mã Lệnh DVS TEAM
 apt-get update -y
 bash <(curl -Ls https://raw.githubusercontent.com/dvsteam/DVS/main/X-UI-DVS-TEAM.sh)
@@ -16,15 +16,15 @@ echo "》『Mở Port: 443』 "
 ufw allow 443
 echo "》『Mở Port: 80』 "
 ufw allow 80
-echo -e "${tim}════════════════════════════════════════  ${plain}"           
-echo -e "${vang}✔•DVS TEAM Đã Hoàn Tất Cài Đặt X-UI•✔ ${plain}"  
-echo -e "${tim}════════════════════════════════════════  ${plain}"
+echo -e "${tim}════════════════════════════════════════════════════    ${plain}"           
+echo -e "${vang}✔•DVS TEAM Đã Hoàn Tất Cài Đặt X-UI•✔ ${plain}" 
+echo -e "${tim}════════════════════════════════════════════════════    ${plain}"
 echo -e "${xanhluc}1.Truy Cập Vào Trình duyệt ${tim}(Chrome)${plain} hoặc ${tim}(Cốc Cốc) ${plain}"
-echo -e "2.Nhập『Địa Chỉ IPv4』của VPS hoặc『Tên Miền』Thêm Đuôi ${tim}(:54321)${plain}  "
+echo -e "2.Nhập『Địa Chỉ IPv4』của VPS hoặc『Tên Miền』Thêm Đuôi "${tim}:54321${plain}"  "
 echo -e "➩Tài Khoảng(Username) » ${tim}admin ${plain}  "
 echo -e "➩Mật Khẩu(Password)   » ${tim}admin ${plain}  "
-echo -e "${tim}════════════════════════════════════════ ${plain}"
+echo -e "${tim}════════════════════════════════════════════════════    ${plain}"
 echo -e "${lam}✘Sau Khi Đăng Nhập Vui Lòng Đổi (Mật Khẩu) để tránh bị lộ thông tin. "
-echo -e "➢Tham Gia Nhóm Hỗ Trợ Tại: https://vpn.dvsteam.tk  "
-echo -e "▂ ▄ ▅ ▇【Copyright © DVS TEAM】▇ ▆ ▄ ▂    "
-echo -e "${tim}════════════════════════════════════════ ${plain}"
+echo -e "➢Tham Gia Nhóm Hỗ Trợ Tại: ${vang}https://vpn.dvsteam.tk ${plain} "
+echo -e "▂ ▄ ▅ ▇【Copyright © DVS TEAM】▇ ▆ ▄ ▂   "
+echo -e "${tim}════════════════════════════════════════════════════    ${plain}"
