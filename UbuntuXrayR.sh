5b63d3439782142cf9efceb3a2124c301cb8dad6
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2021-11-30 22:52:25 +0700

    Xray V2board 1nháy

---
+++ UbuntuXrayR.sh
@@ -0,0 +1,24 @@
+# Ubuntu XrayR V2board DVS TEAM
+sudo apt-get update
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+systemctl enable docker
+# Cài Đặt Docker
+curl -fsSL https://get.docker.com | bash -s docker
+curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+# Cài Đặt Xray DVSTEAM
+git clone https://github.com/XrayR-project/XrayR-release
+cd XrayR-release/config && vi config.yml
+# DVSTEAM 1CLICK
